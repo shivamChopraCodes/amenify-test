a349de8458a2fc40afa29e3cc667341bfabf525e
shivamChopraCodes <shivam1112010@gmail.com>
2022-09-07 17:04:14 +0000

    redux, react router setup

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
+import { useSelector } from 'react-redux';
 import './App.css';
-import UserGreet from './Components/UserGreet';
-import ServicesDisplayed from './Components/ServicesDisplayed';
-import Testimonial from './Components/Testimonial';
-import MenuSection from './Components/MenuSection';
-import Carousel from './Components/Carousel';
-import Footer from './Components/Footer';
-import Navbar from './Components/Navbar';
 
 function App() {
+  const name = useSelector(state=>state.name)
+  console.log(name);
   return (
-    <div className='metropolis w-full max-w-screen-sm flex-col-center mx-auto px-4 pb-20 relative'>
-      <UserGreet />
-      <ServicesDisplayed />
-      <Testimonial />
-      <MenuSection />
-      <Carousel />
-      <Footer />
-      <Navbar />
-      <img src='/images/message-icon.svg' className='fixed right-1 bottom-24 z-20' alt={'message icon'} />
+    <div className='metropolis w-full max-w-screen-sm flex-col-center mx-auto px-4 pb-20 relative '>
+      <p className='uppercase text-xl ' >hi</p>
     </div>
   );
 }
---
+++ src/redux/reducers/index.js
@@ -0,0 +1,7 @@
+import { combineReducers } from "redux";
+
+const rootReducer=combineReducers({
+    name: (state)=>'test',
+})
+
+export default rootReducer
